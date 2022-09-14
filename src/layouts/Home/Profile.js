939b8d5bfd02ccca7e9509133b4d4a62a225c36a
Daniel Andrada <72061244+DaniAndrada1980@users.noreply.github.com>
2022-09-14 14:49:12 -0300

    Add files via upload

---
+++ src/layouts/Home/Profile.js
@@ -0,0 +1,108 @@
+import profileKatakana from 'assets/katakana-profile.svg?url';
+import profileImgLarge from 'assets/profile-large.jpg';
+import profileImgPlaceholder from 'assets/profile-placeholder.jpg';
+import profileImg from 'assets/profile.jpg';
+import { Button } from 'components/Button';
+import { DecoderText } from 'components/DecoderText';
+import { Divider } from 'components/Divider';
+import { Heading } from 'components/Heading';
+import { Image } from 'components/Image';
+import { Link } from 'components/Link';
+import { Section } from 'components/Section';
+import { Text } from 'components/Text';
+import { Transition } from 'components/Transition';
+import { Fragment, useState } from 'react';
+import { media } from 'utils/style';
+import styles from './Profile.module.css';
+
+const ProfileText = ({ visible, titleId }) => (
+  <Fragment>
+    <Heading className={styles.title} data-visible={visible} level={3} id={titleId}>
+      <DecoderText text="Acerca de Mi" start={visible} delay={500} />
+    </Heading>
+    <Text className={styles.description} data-visible={visible} size="l" as="p">
+      Mi nombre es Daniel, Estoy capacitándome como Full Stack Developer en OpenBootCamp,
+      el mejor lugar para aprender y también como Android Developer en Google Academy,
+      aprendí en el pasado de manera autodidacta y busco certificar lo que se hacer,
+      programar en ReactJS, Bootstrap HTML, CSS , JS , JAVA y/o React Native para Android,
+      y además estoy aprendiendo Solidity y desarrollo en Ethereum por mi cuenta,
+      soy autodidacta por naturaleza aunque poseo también una Diplomatura Universitaria
+      en Programación de Mainframes (Cobol, JCL, DB2) ,de la Universidad Nacional de
+      Tres de Febrero(Argentina), además soy autodidacta en el área de seguridad informática,
+      manejo distros de Linux estilo Kali Linux y sus componentes y herramientas,
+      manejo bien el lenguaje Ingles.
+
+    </Text>
+
+  </Fragment>
+);
+
+export const Profile = ({ id, visible, sectionRef }) => {
+  const [focused, setFocused] = useState(false);
+  const titleId = `${id}-title`;
+
+  return (
+    <Section
+      className={styles.profile}
+      onFocus={() => setFocused(true)}
+      onBlur={() => setFocused(false)}
+      as="section"
+      id={id}
+      ref={sectionRef}
+      aria-labelledby={titleId}
+      tabIndex={-1}
+    >
+      <Transition in={visible || focused} timeout={0}>
+        {visible => (
+          <div className={styles.content}>
+            <div className={styles.column}>
+              <ProfileText visible={visible} titleId={titleId} />
+              <Button
+                secondary
+                className={styles.button}
+                data-visible={visible}
+                href="/contact"
+                icon="send"
+              >
+                Contacto
+              </Button>
+            </div>
+            <div className={styles.column}>
+              <div className={styles.tag} aria-hidden>
+                <Divider
+                  notchWidth="64px"
+                  notchHeight="8px"
+                  collapsed={!visible}
+                  collapseDelay={1000}
+                />
+                <div className={styles.tagText} data-visible={visible}>
+
+                </div>
+              </div>
+              <div className={styles.image}>
+                <Image
+                  reveal
+                  delay={100}
+                  placeholder={profileImgPlaceholder}
+                  srcSet={[profileImg, profileImgLarge]}
+                  sizes={`(max-width: ${media.mobile}px) 100vw, 480px`}
+                  alt=""
+                />
+                <svg
+                  aria-hidden="true"
+                  width="135"
+                  height="765"
+                  viewBox="0 0 135 765"
+                  className={styles.svg}
+                  data-visible={visible}
+                >
+                  <use href={``} />
+                </svg>
+              </div>
+            </div>
+          </div>
+        )}
+      </Transition>
+    </Section>
+  );
+};
